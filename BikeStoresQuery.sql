fa19756d7887c80cd06c65403e65cb28b3b6203f
RamziLahouegue <115560082+RamziLahouegue@users.noreply.github.com>
2024-01-05 16:18:06 +0100

    SQL Queries

---
+++ BikeStoresQuery.sql
@@ -0,0 +1,38 @@
+select 
+	ord.order_id,
+	CONCAT(cus.first_name,' ', cus.last_name) as 'customers',
+	cus.city,
+	cus.state,
+	ord.order_date,
+	sum(ite.quantity) as 'total_units',
+	sum(ite.quantity * ite.list_price) as 'revenue',
+	pro.product_name,
+	cat.category_name,
+	sto.store_name,
+	concat(sta.first_name,' ', sta.last_name) as 'sales_rep'
+from sales.orders ord
+join sales.customers cus
+on ord.customer_id = cus.customer_id
+join sales.order_items ite
+on ord.order_id = ite.order_id
+join production.products pro
+on ite.product_id = pro.product_id
+join production.categories cat
+on pro.category_id = cat.category_id
+join sales.stores sto
+on ord.store_id = sto.store_id
+join sales.staffs sta
+on ord.staff_id = sta.staff_id
+group by 
+	ord.order_id,
+	CONCAT(cus.first_name,' ', cus.last_name),
+	cus.city,
+	cus.state,
+	ord.order_date,
+	pro.product_name,
+	cat.category_name,
+	sto.store_name,
+	concat(sta.first_name,' ', sta.last_name)
+
+---Dataset table looks good in SQL, no need to clean. No redundancies and no unwanted fields.
+---To connect to excel, either 1)copy and paste table, or 2)connect database to excel workbook and import dataset into worksheet => Creates connection between workbook and database, so that whatever future changes to the database are automatically reflected in excel 
